f8b22f77ea353f5b8d104d17edcfd81e4d64710b
ZivaiiKane <zivaiikane@gmail.com>
2022-04-16 20:54:59 +0200

    feat: :tada: Initial commit
    Initial project commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'brighttrips',
+    appId: '1:503165627676:web:8fd27e29cc3823ae5b5ed9',
+    storageBucket: 'brighttrips.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDYxQSYNB1-wXzLAbCaRYejmrjODU-JB6s',
+    authDomain: 'brighttrips.firebaseapp.com',
+    messagingSenderId: '503165627676',
+  },
   production: true
 };
